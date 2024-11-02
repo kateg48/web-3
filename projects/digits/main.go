d6fb30a2d0100086ca8017e2cbdda9a994748bde
kate g <zabzakate@gmail.com>
2024-11-02 22:14:07 +0300

    E.B.Gappova

--- projects/digits/main.go
+++ projects/digits/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var s string
+	fmt.Scan(&s)
+	var max byte
+	for i := 0; i < len(s); i++ {
+		if s[i] >= max {
+			max = s[i]
+		}
+	}
+	fmt.Printf("%c", max)
 }
--- projects/star/main.go
+++ projects/star/main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var s string
+	fmt.Scan(&s)
+	for i := 0; i < len(s); i++ {
+		if i != len(s)-1 {
+			fmt.Printf("%c", s[i])
+			fmt.Print("*")
+		} else {
+			fmt.Printf("%c", s[i])
+		}
+	}
 }
